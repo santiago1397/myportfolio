1a286d7264a58497ce320b6b02244a9cbb71b41c
santiago <santiagovillahermosa@gmail.com>
2022-07-26 21:13:15 -0400

    9 commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,16 @@ import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects  from '../components/Projects'
 import Contact from '../components/Contact'
-import { useInView } from 'react-intersection-observer'
 
 const Home: NextPage = () => {
-  const [section,sectionInView] = useInView({threshold: 0.5})
   return (
     <div className={styles.container}>
       <Hero />
-      <Navbar view={sectionInView}/>
+      <Navbar />
       <About />
       <Skills />
       <Projects />
-      <Contact  ref={section}/>
+      <Contact  />
     </div>
   )
 }
